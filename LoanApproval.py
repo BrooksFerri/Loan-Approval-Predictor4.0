480f3c00d488ae773509ea7325951b03aee3458d
naolson2 <naolson2@ncsu.edu>
2025-12-07 11:43:50 -0500

    Update LoanApproval.py

--- LoanApproval.py
+++ LoanApproval.py
@@ -15,8 +15,7 @@ st.markdown("""
     <style>
     /* White background and clean styling */
     .stApp {
-        color: 0a2463;
-        background-color: #ffffff;
+        background-color: #000000;
     }
     
     /* Modern title styling */
